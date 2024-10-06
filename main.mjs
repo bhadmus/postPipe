6e16edeff49da205c62e000f4638f97cbdd0325c
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-10-06 20:33:47 +0100

    fixed bad name

--- main.mjs
+++ main.mjs
@@ -10,7 +10,7 @@ import {
   makeCommit,
   exportPostmanCollection,
   createRepo,
-  verifyGithubActionsWorkPipe,
+  verifyGithubActionsWorkflow,
   initializeRepoWithReadme,
   readmeExists,
   createAndInstallPackageJson,
